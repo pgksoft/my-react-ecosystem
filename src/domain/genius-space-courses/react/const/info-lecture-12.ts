b7a3cc79c73d5f5518415db0515fcdc9688a3b18
pgksoft <pgksoft@gmail.com>
2025-01-10 00:14:48 +0200

    three useful links was added

--- src/domain/genius-space-courses/react/const/info-lecture-12.ts
+++ src/domain/genius-space-courses/react/const/info-lecture-12.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import TInfoLecture from '../../type/info-lecture';
 
 const infoLecture12: TInfoLecture = {
@@ -14,6 +15,19 @@ const infoLecture12: TInfoLecture = {
     {
       title: 'Redux Toolkit Quick Start',
       href: 'https://redux-toolkit.js.org/tutorials/quick-start'
+    },
+    {
+      title: 'Introduction to Immer',
+      href: 'https://immerjs.github.io/immer/'
+    },
+    {
+      title: 'Article: Data immutability with Immer',
+      href:
+        'https://medium.com/workleap/data-immutability-with-immer-e024538acb80#:~:text=Immer%20is%20a%20JavaScript%20library%20that%20ensures%20the,new%20syntax%20to%20learn%20to%20work%20with%20it.'
+    },
+    {
+      title: 'Redux DevTools',
+      href: 'https://github.com/reduxjs/redux-devtools?tab=readme-ov-file'
     }
   ]
 };
